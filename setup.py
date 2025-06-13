f04f1db7966e3267923b475641003a383e33b1c4
kroskros1 <kros4bros@gmail.com>
2025-06-13 04:10:46 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="window-bot",
+    version="1.0",
+    install_requires=[
+        "python-telegram-bot==20.3",
+        "matplotlib==3.8.0",
+        "numpy==1.26.0",
+        "python-dotenv==1.0.0"
+    ],
+)
