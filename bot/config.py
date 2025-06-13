f7b77d25e5c95eac75927644830233d0cd99e84e
kroskros1 <kros4bros@gmail.com>
2025-06-13 04:23:43 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 PORT = int(os.getenv('PORT', 8443))
-ADMIN_IDS = list(map(int, os.getenv('ADMIN_IDS', '').split(',')))
+
+# Виправлений код для ADMIN_IDS
+admin_ids_str = os.getenv('ADMIN_IDS', '')
+ADMIN_IDS = [int(id_str) for id_str in admin_ids_str.split(',') if id_str.strip()] if admin_ids_str else []
