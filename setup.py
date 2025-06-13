1b1eb9cac17ff02d74a3d649d98b56a622998b84
kroskros1 <kros4bros@gmail.com>
2025-06-13 04:13:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     install_requires=[
         "python-telegram-bot==20.3",
         "matplotlib==3.8.0",
-        "numpy==1.26.0",
+        "numpy==1.24.4",  # Змінено версію
         "python-dotenv==1.0.0"
     ],
+    python_requires='>=3.9, <3.11',  # Додано обмеження
 )
