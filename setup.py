f48a6857091ef1a8d19f9fda502beb7614dcfb1d
kroskros1 <kros4bros@gmail.com>
2025-06-13 04:17:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="window-bot",
     version="1.0",
     install_requires=[
-        "python-telegram-bot==20.3",
-        "matplotlib==3.8.0",
-        "numpy==1.24.4",  # Змінено версію
-        "python-dotenv==1.0.0"
+        "python-telegram-bot>=20.3",
+        "matplotlib>=3.7.1",
+        "numpy>=1.24.3",
+        "python-dotenv>=1.0.0"
     ],
-    python_requires='>=3.9, <3.11',  # Додано обмеження
+    python_requires='>=3.10, <3.11',
 )
